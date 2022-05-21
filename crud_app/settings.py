d694bafe11ce9b185e778352ffb47d05d80e5f2d
Krystian <devcode.biel@gmail.com>
2022-05-21 23:38:26 +0200

    Eight commit

--- crud_app/settings.py
+++ crud_app/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
